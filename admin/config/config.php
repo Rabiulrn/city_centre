b37c3a271c1b433f5c8837cce745aa0e2a6d14f3
Md. Mottaleb Jeabon <110931950+Mottaleb-Jeabon171@users.noreply.github.com>
2025-10-09 12:14:58 +0600

    Changed

--- admin/config/config.php
+++ admin/config/config.php
@@ -4,14 +4,14 @@
 	    '::1'
 	);
 	date_default_timezone_set("Asia/Dhaka");
-	define("DB_NAME", "xl_software11"); 	 // Database name
+	define("DB_NAME", "city_centre"); 	 // Database name
 	define("HOST", "localhost"); 			// Database host
 	define("USER", "root"); 			   // Database username
 
 	if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
 	    define("PASS", "");  			 // Database password (empty for localhost)
 	} else {
-		define("PASS", "webdev@2022");
+		define("PASS", "admin@267#");
 	}
 
 	
